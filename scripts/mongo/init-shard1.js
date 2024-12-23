f18725f7f0d050e25a0792ea2b51fd6cb7588554
brunopfohl <bruno.pfohl@seznam.cz>
2024-12-23 17:24:27 +0100

    save

--- scripts/mongo/init-shard1.js
+++ scripts/mongo/init-shard1.js
@@ -1,10 +1,8 @@
-rs.initiate(
-  {
+rs.initiate({
     _id: "shard1rs",
     members: [
-      { _id : 0, host : "shard1-1:27017" },
-      { _id : 1, host : "shard1-2:27017" },
-      { _id : 2, host : "shard1-3:27017" }
+        { _id: 0, host: "shard1-1:27018" },
+        { _id: 1, host: "shard1-2:27018" },
+        { _id: 2, host: "shard1-3:27018" }
     ]
-  }
-)
+})
--- scripts/mongo/init-shard2.js
+++ scripts/mongo/init-shard2.js
@@ -1,10 +1,8 @@
-rs.initiate(
-  {
+rs.initiate({
     _id: "shard2rs",
     members: [
-      { _id : 0, host : "shard2-1:27017" },
-      { _id : 1, host : "shard2-2:27017" },
-      { _id : 2, host : "shard2-3:27017" }
+        { _id: 0, host: "shard2-1:27018" },
+        { _id: 1, host: "shard2-2:27018" },
+        { _id: 2, host: "shard2-3:27018" }
     ]
-  }
-)
+})
