de41afe0a55065fa72e173297c803cb83af19bd9
brunopfohl <bruno.pfohl@seznam.cz>
2024-12-27 01:15:17 +0100

    fix

--- scripts/mongo/init.js
+++ scripts/mongo/init.js
@@ -3,5 +3,5 @@ db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE)
 db.createUser({ 
     user: process.env.DB_USERNAME,
     pwd: process.env.DB_PASSWORD,
-    roles: [{ role: 'readWrite', db: process.env.MONGO_INITDB_DATABASE }] 
+    roles: [{ role: 'root', db: process.env.MONGO_INITDB_DATABASE }] 
 }, { w: 'majority', wtimeout: 5000 });
