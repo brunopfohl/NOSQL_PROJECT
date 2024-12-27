e9cb96aec5ddf548655884aee41e1bcfa8f3c24f
Bruno <bruno.pfohl@seznam.cz>
2024-12-27 17:03:39 +0100

    clean up

--- scripts/mongo/init-configserver.js
+++ scripts/mongo/init-configserver.js
@@ -3,9 +3,9 @@ try {
         _id: "cfgrs",
         configsvr: true,
         members: [
-            { _id : 0, host : "cfgsvr1:27019", "priority": 1 },
-            { _id : 1, host : "cfgsvr2:27019", "priority": 0.5 },
-            { _id : 2, host : "cfgsvr3:27019", "priority": 0.5 }
+            { _id : 0, host : "cfgsvr1:27019", priority: 1 },
+            { _id : 1, host : "cfgsvr2:27019", priority: 0.5 },
+            { _id : 2, host : "cfgsvr3:27019", priority: 0.5 }
         ]
     },
     {
